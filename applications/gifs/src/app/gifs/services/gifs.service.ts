e4f700b7bfc4e2504882e3d0799a5f80d9707260
apausa <pabloapausachamorro@MacBook-Pro-de-Pablo.local>
2021-12-02 17:54:52 +0100

    Introduce observables

--- applications/gifs/src/app/gifs/services/gifs.service.ts
+++ applications/gifs/src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private API: string = `https://api.giphy.com/v1/gifs/search`
+  private API_KEY: string = `hsgPiDQDbNOdm8iqgbGb9MDaWPtNZopg`;
   private _history: string[] = [];
   
-  get history() {
-    return [...this._history]
-  }
+  constructor(private http: HttpClient) {};
+  get history() { return [...this._history] }
 
   search(query: string) {
+    query = query.trim().toLocaleLowerCase();
     this._history.unshift(query);
     this._history = this._history.splice(0, 10);
-    console.log(this._history)
+    this.http
+      .get(`${this.API}?api_key=${this.API_KEY}&q=${query}&limit=10`)
+      .subscribe( response => { console.log(response); })
   }
 }
