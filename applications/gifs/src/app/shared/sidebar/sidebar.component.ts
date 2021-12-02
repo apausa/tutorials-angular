bbc6557de2a3c1335cb12501df86ea84d7acf822
apausa <pabloapausachamorro@MacBook-Pro-de-Pablo.local>
2021-12-02 16:17:39 +0100

    Render service items

--- applications/gifs/src/app/shared/sidebar/sidebar.component.ts
+++ applications/gifs/src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SidebarComponent {
+  constructor( private gifsService: GifsService) { };
+  get history() { return this.gifsService.history };
 }
