f585748ecdbb50c0a86c135c6d24441db5b6e408
apausa <pabloapausachamorro@MBP-de-Pablo.home>
2021-12-03 14:56:04 +0100

    Structure countries project

---
+++ applications/countries/src/app/countries/countries.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CapitalComponent } from './pages/capital/capital.component';
+import { CountryComponent } from './pages/country/country.component';
+import { RegionComponent } from './pages/region/region.component';
+import { DetailsComponent } from './pages/details/details.component';
+
+
+
+@NgModule({
+  declarations: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
