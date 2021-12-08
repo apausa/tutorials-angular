728c9574894a2345b4cf6db444d1d23666df6872
apausa <pabloapausachamorro@MBP-de-Pablo.home>
2021-12-08 18:00:16 +0100

    Execute RouterModule

--- applications/02/src/app/app-routing.module.ts
+++ applications/02/src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+import { CountryComponent } from './countries/pages/country/country.component';
+import { CapitalComponent } from './countries/pages/capital/capital.component';
+import { RegionComponent } from './countries/pages/region/region.component';
+import { DetailsComponent } from './countries/pages/details/details.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CountryComponent, 
+    pathMatch: 'full'
+  },
+  {
+    path: 'capital',
+    component: CapitalComponent,
+  },
+  {
+    path: 'region',
+    component: RegionComponent, 
+  },
+  {
+    path: 'country/:id',
+    component: DetailsComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
