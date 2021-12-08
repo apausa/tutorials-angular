4b824eaafc889f2132df45e62a90945020e28a3c
apausa <pabloapausachamorro@MBP-de-Pablo.home>
2021-12-08 18:19:05 +0100

    Create menu component
    Create footer component

--- applications/02/src/app/shared/shared.module.ts
+++ applications/02/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    MenuComponent,
+    FooterComponent
   ],
   exports: [
     SidebarComponent,
