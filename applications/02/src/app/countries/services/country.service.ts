19fa2d3f8c14a4a5ec5feb8091b662908e3800cb
apausa <pabloapausachamorro@MBP-de-Pablo.home>
2021-12-08 19:36:15 +0100

    Configure countries service

--- applications/02/src/app/countries/services/country.service.ts
+++ applications/02/src/app/countries/services/country.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
+  private COUNTRIES_API: string = "https://restcountries.com/v3.1";
+  constructor( private http: HttpClient ) { }
 
-  constructor() { }
+  searchCountry(term: string) {
+    const url = `${ this.COUNTRIES_API }/name/${ term }`;
+    return this.http.get( url )
+  }
 }
+
